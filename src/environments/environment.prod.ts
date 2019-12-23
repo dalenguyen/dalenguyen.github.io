09eaeeec3660e03c0fc7e73a6c0bea31466d7453
Dale Nguyen <dungnq@itbox4vn.com>
2019-12-23 12:54:21 -0500

    feat(blog): added buttercms package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 import { versions } from './versions'
-import * as pkg from '../../package.json';
+import * as pkg from '../../package.json'
 
 export const environment = {
   production: true,
   version: pkg.version,
-  gitHash: versions.revision
-};
+  gitHash: versions.revision,
+  butterCMS: '10de8a1782f01676902398495c4062893956ac9c'
+}
