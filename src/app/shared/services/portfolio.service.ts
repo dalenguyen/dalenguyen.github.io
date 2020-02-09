c7febedc4b3397c73e50dcea1fa9621fe94827e4
Dale Nguyen <dungnq@itbox4vn.com>
2020-02-09 18:51:07 -0500

    Updated portfolio service

--- src/app/shared/services/portfolio.service.ts
+++ src/app/shared/services/portfolio.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core'
-import { captureException } from '@sentry/core'
 import { HttpClient } from '@angular/common/http'
-import { GitProject } from '../models/git.project'
-import { Observable, of } from 'rxjs'
+
+import { Observable } from 'rxjs'
+import { captureException } from '@sentry/core'
 import { map, publishReplay, refCount, catchError } from 'rxjs/operators'
+
+import { GitProject } from '../models/git.project'
 
 @Injectable({
   providedIn: 'root'
@@ -25,24 +27,17 @@ export class PortfolioService {
   gitBaseUrl = 'https://api.github.com/users/dalenguyen/repos?per_page=100'
 
   constructor(private http: HttpClient) {
-    this.projects$ = this.getGitProjects()
+    this.getGitProjects()
   }
 
-  getGitProjects(): Observable<GitProject[]> {
-    if (!this.projects$) {
-      this.projects$ = this.http.get<GitProject[]>(this.gitBaseUrl).pipe(
-        map(projects =>
-          projects.filter(project => this.gitProjects.includes(project.name))
-        ),
-        publishReplay(1),
-        refCount(),
-        catchError(error => captureException(error))
-      ) as Observable<GitProject[]>
-    }
-    return this.projects$
-  }
-
-  clearCache() {
-    this.projects$ = of([])
+  getGitProjects(): void {
+    this.projects$ = this.http.get<GitProject[]>(this.gitBaseUrl).pipe(
+      map(projects =>
+        projects.filter(project => this.gitProjects.includes(project.name))
+      ),
+      publishReplay(1),
+      refCount(),
+      catchError(error => captureException(error))
+    ) as Observable<GitProject[]>
   }
 }
