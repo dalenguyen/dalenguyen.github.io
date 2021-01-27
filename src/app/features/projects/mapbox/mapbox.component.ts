bf5c999eb95e9f804cf4bf196dd2af93424ea3b7
Dale Nguyen <dungnq@itbox4vn.com>
2021-01-26 22:59:06 -0500

    feat(map): added map configuration

--- src/app/features/projects/mapbox/mapbox.component.ts
+++ src/app/features/projects/mapbox/mapbox.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-mapbox',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapboxComponent implements OnInit {
 
+  map: mapboxgl.Map
+  style = 'mapbox://styles/mapbox/streets-v11'
+  lat = 37.75
+  lng = -122.41
+
   constructor() { }
 
   ngOnInit(): void {
+    this.map = new mapboxgl.Map({
+      accessToken: environment.mapbox.accessToken,
+      container: 'map',
+      style: this.style,
+      zoom: 2,
+      center: [this.lng, this.lat],
+    })
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl())
   }
 
 }
