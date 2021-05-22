ddacc4d9c46f7c6843acfb44d1d86e3961d6968f
Dale Nguyen <dungnq@itbox4vn.com>
2021-05-22 10:49:55 -0400

    fixed postcss error after migrating to Angular v12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 import { versions } from './versions'
-import * as pkg from '../../package.json'
+import packageInfo from '../../package.json'
 
 export const environment = {
   production: false,
-  version: pkg.version,
+  version: packageInfo.version,
   gitHash: versions.revision,
   butterCMSToken: '10de8a1782f01676902398495c4062893956ac9c',
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiZGFsZW5ndXllbiIsImEiOiJja2tlamhhNjcwMHVyMnFvNHJtNHhjZ2RyIn0.Lm3dJG12k3-LmOu_G5y-qA'
-  }
+    accessToken: 'pk.eyJ1IjoiZGFsZW5ndXllbiIsImEiOiJja2tlamhhNjcwMHVyMnFvNHJtNHhjZ2RyIn0.Lm3dJG12k3-LmOu_G5y-qA',
+  },
 }
 
 /*
