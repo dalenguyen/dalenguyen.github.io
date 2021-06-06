07e7990882186c5b4094af3bc2aa1ed98e963a18
Dale Nguyen <dungnq@itbox4vn.com>
2021-06-06 11:08:00 -0400

    feat(chat): added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  prefix: '',
+  purge: {
+    content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        navBar: '232px',
+      },
+      height: {
+        topBar: '60px',
+        nowPlayingBar: '90px',
+      },
+      colors: {
+        primary: '#1db954',
+        sliderRail: '#535353',
+        sliderTrack: '#b3b3b3',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
+  plugins: [],
+}
