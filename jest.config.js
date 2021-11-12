7c609034213494f81e08c31f0f4ec25051585d6e
Dale Nguyen <dungnq@itbox4vn.com>
2021-11-12 17:24:55 -0500

    feat(nx): migrated to nx v13

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/portfolio',
-    '<rootDir>/libs/material',
-    '<rootDir>/apps/saas',
-    '<rootDir>/libs/saas-libs',
-    '<rootDir>/apps/chat',
-    '<rootDir>/apps/api-products',
-    '<rootDir>/libs/openai',
-  ],
-}
+const { getJestProjects } = require('@nrwl/jest')
+
+module.exports = { projects: getJestProjects() }
