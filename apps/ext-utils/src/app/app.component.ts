bf79f1207f07505bb3072187053a391ac71cc8a4
Dale Nguyen <dungnq@itbox4vn.com>
2021-11-27 23:29:25 -0500

    feat(ext): updated style

--- apps/ext-utils/src/app/app.component.ts
+++ apps/ext-utils/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   changeBackground() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs: { id?: number }[]) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0 && tabs[0].id != null) {
         chrome.tabs.executeScript(tabs[0].id, {
           code: 'document.body.style.backgroundColor = "red";',
