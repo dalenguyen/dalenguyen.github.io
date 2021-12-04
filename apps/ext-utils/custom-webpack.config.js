e5cd1371e8fb1ef456425dd29f2d2d1a3749af02
Dale Nguyen <dungnq@itbox4vn.com>
2021-12-04 11:14:19 -0500

    feat(chrome): add menuContext

--- apps/ext-utils/custom-webpack.config.js
+++ apps/ext-utils/custom-webpack.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  entry: { background: 'apps/ext-utils/src/background.ts' },
+  entry: {
+    background: 'apps/ext-utils/src/background.ts',
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
 }
--- apps/ext-utils/src/background.ts
+++ apps/ext-utils/src/background.ts
@@ -1,8 +1,22 @@
 /// <reference types="chrome"/>
-
 const color = '#3aa757'
 
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'searchForWord',
+    title: 'Search DuckDuckGo for: %s',
+    contexts: ['selection'],
+  })
+
   chrome.storage.sync.set({ color })
   console.log('Default background color set to %cgreen', `color: ${color}`)
 })
+
+const searchForWord = (info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab | undefined) => {
+  console.log('Word ' + info.selectionText + ' was clicked.')
+  chrome.tabs.create({
+    url: 'https://duckduckgo.com/?q=' + info.selectionText,
+  })
+}
+
+chrome.contextMenus.onClicked.addListener(searchForWord)
