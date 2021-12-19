25448bbbb26be0fff3adee633f8c306fbd9b7d4e
Dale Nguyen <dungnq@itbox4vn.com>
2021-12-19 12:09:36 -0500

    migrate to nx v13.4.0

--- apps/portfolio/src/app/home/portfolio/portfolio.component.ts
+++ apps/portfolio/src/app/home/portfolio/portfolio.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener } from '@angular/core';
-import { PortfolioService } from '../../shared/services/portfolio.service';
+import { Component } from '@angular/core'
+import { PortfolioService } from '../../shared/services/portfolio.service'
 
 @Component({
   selector: 'app-portfolio',
@@ -7,26 +7,5 @@ import { PortfolioService } from '../../shared/services/portfolio.service';
   styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-  // watcher: Subscription
-  cols = 4;
-
-  @HostListener('window:resize', [])
-  private onResize() {
-    const screenSize = window.innerWidth;
-    if (screenSize > 1450) {
-      this.cols = 4;
-    } else if (screenSize <= 1450 && screenSize > 1280) {
-      this.cols = 3;
-    } else if (screenSize <= 1280 && screenSize > 980) {
-      this.cols = 2;
-    } else {
-      this.cols = 1;
-    }
-  }
-
-  // @ViewChild('grid') grid: MatGridList
-
-  constructor(public portfolioService: PortfolioService) {
-    this.onResize();
-  }
+  constructor(public portfolioService: PortfolioService) {}
 }
