bb9d4f14e97e2959cc39eec52fd2a3d791fca2a3
microsoftbuild <38135549+microsoftbuild@users.noreply.github.com>
2021-12-23 20:33:48 +0530

    Added engines: 'cushman-codex' and 'davinci-codex'

--- libs/openai/src/models/engine.model.ts
+++ libs/openai/src/models/engine.model.ts
@@ -1,8 +1,10 @@
 export enum EngineName {
   Davinci = 'davinci',
+  DavinciCodex = 'davinci-codex',
   DavinciInstructBeta = 'davinci-instruct-beta ',
   Curie = 'curie',
   CurieInstructBeta = 'curie-instruct-beta',
+  CushmanCodex = 'cushman-codex',
   Babbage = 'babbage',
   Ada = 'ada',
 }
