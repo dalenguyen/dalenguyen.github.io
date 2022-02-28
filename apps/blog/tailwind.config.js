c4a09e6cd5d1eada74502dddfc01dfbb109b027e
Dale Nguyen <dale@dalenguyen.me>
2022-02-27 22:51:26 -0500

    feat(blog): add tailwindcss

---
+++ apps/blog/tailwind.config.js
@@ -0,0 +1,16 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind')
+const { join } = require('path')
+
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset.js')],
+  purge: [join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'), ...createGlobPatternsForDependencies(__dirname)],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
