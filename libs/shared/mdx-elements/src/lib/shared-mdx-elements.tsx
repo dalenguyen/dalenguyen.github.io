aaa4de3dc4a9170d167f6292389c39669be303c2
Dale Nguyen <dale@dalenguyen.me>
2022-03-03 16:10:25 -0500

    feat(blog): add custom link with mdx

--- libs/shared/mdx-elements/src/lib/shared-mdx-elements.tsx
+++ libs/shared/mdx-elements/src/lib/shared-mdx-elements.tsx
@@ -1,20 +1,22 @@
 import dynamic from 'next/dynamic';
+import { CustomLink } from './custom-link/custom-link';
 import './shared-mdx-elements.module.scss';
 
 
 export const mdxElements = {
+  a: CustomLink,
   Youtube: dynamic(() => import('./youtube/youtube')),
 }
 
-/* eslint-disable-next-line */
-export interface SharedMdxElementsProps {}
+// /* eslint-disable-next-line */
+// export interface SharedMdxElementsProps {}
 
-export function SharedMdxElements(props: SharedMdxElementsProps) {
-  return (
-    <div>
-      <h1>Welcome to SharedMdxElements!</h1>
-    </div>
-  )
-}
+// export function SharedMdxElements(props: SharedMdxElementsProps) {
+//   return (
+//     <div>
+//       <h1>Welcome to SharedMdxElements!</h1>
+//     </div>
+//   )
+// }
 
-export default SharedMdxElements
+// export default SharedMdxElements
