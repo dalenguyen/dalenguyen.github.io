6c6fedd680ba84b4eb284cc380def208e903cd35
Dale Nguyen <dale@dalenguyen.me>
2022-08-23 17:33:36 -0400

    chore: clean commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,5 @@ module.exports = {
   rules: {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
-    'scope-enum': [2, 'always', ['vite-angular-plugin', 'create-analog']],
   },
 }
