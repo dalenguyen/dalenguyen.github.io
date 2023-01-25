ffc315a53c4af3a803fc445bcf12fe9de828912b
dalenguyen <dale@dalenguyen.me>
2023-01-26 00:00:24 +0700

    chore: add shared dep for module federation

--- apps/resume-remote/webpack.prod.config.js
+++ apps/resume-remote/webpack.prod.config.js
@@ -2,6 +2,11 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation')
 const config = require('./module-federation.config')
 module.exports = withModuleFederation({
   ...config,
+  shared: {
+    '@angular/core': { singleton: true, strictVersion: true, eager: true },
+    '@angular/material/icon': { singleton: true, strictVersion: true, eager: true },
+    '@angular/router/': { singleton: true, strictVersion: true, eager: true },
+  },
   /*
    * Remote overrides for production.
    * Each entry is a pair of an unique name and the URL where it is deployed.
