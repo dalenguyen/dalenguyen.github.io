8ff2cd9f7266920542fcd90ea65ddf7f8702dab2
Dale Nguyen <dale@dalenguyen.me>
2024-04-03 18:28:50 -0400

    remove ng-ssr

--- apps/ng-ssr/src/cp-angular.ts
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-
-;(async () => {
-  const src = '../..dist/apps/portfolio/browser'
-  const copy = './dist'
-
-  await fs.remove(copy)
-  await fs.copy(src, copy)
-})()
