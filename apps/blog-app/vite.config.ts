40592aa0fab17fd893241b3699f64c9c00b6b536
Dale Nguyen <dungnq@itbox4vn.com>
2024-05-18 16:19:00 -0400

    update analog version

--- apps/blog-app/vite.config.ts
+++ apps/blog-app/vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 
 import analog from '@analogjs/platform'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -16,21 +17,25 @@ export default defineConfig(({ mode }) => ({
   },
   server: {
     fs: {
-      allow: ['../../libs/portfolio'],
+      allow: ['.', '../../libs/portfolio'],
     },
   },
   plugins: [
-    tsconfigPaths(),
-    viteCommonjs(),
+    // tsconfigPaths(),
+    // viteCommonjs(),
+    nxViteTsPaths(),
     analog({
+      nitro: {
+        preset: 'vercel',
+      },
       static: true,
       // ssr: true,
       // ssrBuildDir: '../../dist/apps/blog-app/ssr',
       // entryServer: 'apps/blog-app/src/main.server.ts',
-      vite: {
-        tsconfig: 'apps/blog-app/tsconfig.app.json',
-        inlineStylesExtension: 'scss|sass|less',
-      },
+      // vite: {
+      //   tsconfig: 'apps/blog-app/tsconfig.app.json',
+      //   inlineStylesExtension: 'scss|sass|less',
+      // },
     }),
   ],
   test: {
