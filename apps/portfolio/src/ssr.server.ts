0445147cdefe3f00f47715c7b7e14736e75dc514
Dale Nguyen <dungnq@itbox4vn.com>
2024-05-18 10:13:41 -0400

    add angular/cli

--- apps/portfolio/src/ssr.server.ts
+++ apps/portfolio/src/ssr.server.ts
@@ -1,5 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common'
-import { ngExpressEngine } from '@nguniversal/express-engine'
+import { ngExpressEngine } from '@angular/ssr'
 import * as express from 'express'
 import { existsSync } from 'fs'
 import { join } from 'path'
