6b156c74a8dad02c5b84e44bbc5fa442b61f23b1
Dale Nguyen <dale@dalenguyen.me>
2025-03-14 20:09:47 -0400

    chore(nx): remove api-stripe project

--- apps/api-stripe/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  stripe: {
-    secretKey: process.env.STRIPE_SECRET_KEY,
-  },
-}
--- apps/api-stripe/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  stripe: {
-    secretKey: process.env.STRIPE_SECRET_KEY,
-  },
-}
