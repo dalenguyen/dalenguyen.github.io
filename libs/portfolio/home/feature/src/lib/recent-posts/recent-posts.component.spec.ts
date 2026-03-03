554f42a8233f7b0ca728deeae979728fff8b47ba
Dale Nguyen <dale@dalenguyen.me>
2026-03-03 16:28:13 +0700

    chore(portfolio): update recent posts

--- libs/portfolio/home/feature/src/lib/recent-posts/recent-posts.component.spec.ts
+++ libs/portfolio/home/feature/src/lib/recent-posts/recent-posts.component.spec.ts
@@ -1,60 +1,57 @@
-import * as contentModule from '@analogjs/content'
+import { provideHttpClient } from '@angular/common/http'
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { RecentPostsComponent } from './recent-posts.component'
 
-// Add Jest type definitions for the test functions
 declare const describe: any
 declare const beforeEach: any
-declare const jest: any
+declare const afterEach: any
 declare const it: any
 declare const expect: any
 
 describe('RecentPostsComponent', () => {
   let component: RecentPostsComponent
   let fixture: ComponentFixture<RecentPostsComponent>
+  let httpMock: HttpTestingController
 
   beforeEach(async () => {
-    // Mock the injectContentFiles function
-    jest.spyOn(contentModule, 'injectContentFiles').mockReturnValue([
-      {
-        filename: '/src/content/test-post.md',
-        content: '',
-        attributes: {
-          title: 'Test Post',
-          slug: 'test-post',
-          description: 'This is a test post',
-          coverImage: 'https://example.com/test.jpg',
-          categories: ['Test'],
-          published: '2023-05-01',
-          profileImage: 'https://example.com/profile.jpg',
-          author: {
-            firstName: 'John',
-            lastName: 'Doe',
-          },
-        },
-      },
-    ])
-
     await TestBed.configureTestingModule({
       imports: [RecentPostsComponent, RouterTestingModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents()
 
+    httpMock = TestBed.inject(HttpTestingController)
     fixture = TestBed.createComponent(RecentPostsComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
+
+    httpMock.expectOne((req) => req.url.includes('dev.to')).flush([
+      {
+        title: 'Test Post',
+        slug: 'test-post',
+        description: 'A test post',
+        cover_image: 'https://example.com/img.jpg',
+        tag_list: ['angular'],
+        published_at: '2026-01-01T00:00:00Z',
+        url: 'https://dev.to/dalenguyen/test-post',
+        user: { name: 'Dale Nguyen', profile_image: 'https://example.com/avatar.jpg' },
+      },
+    ])
+    fixture.detectChanges()
   })
+
+  afterEach(() => httpMock.verify())
 
   it('should create', () => {
     expect(component).toBeTruthy()
   })
 
-  it('should have recent posts', () => {
-    expect(component.recentPosts.length).toBeGreaterThan(0)
+  it('should display fetched posts', () => {
+    expect(component.recentPosts().length).toBe(1)
   })
 
   it('should limit to 3 recent posts', () => {
-    // Since we're mocking just 1 post, we'll have 1 post
-    expect(component.recentPosts.length).toBeLessThanOrEqual(3)
+    expect(component.recentPosts().length).toBeLessThanOrEqual(3)
   })
 })
