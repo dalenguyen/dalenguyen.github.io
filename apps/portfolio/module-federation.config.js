14ecaf937023047edecb66a4f4deaad176d63ece
Dale Nguyen <dale@dalenguyen.me>
2026-03-03 16:09:52 +0700

    fix(portfolio): update dependencies

--- apps/portfolio/module-federation.config.js
+++ apps/portfolio/module-federation.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   name: 'portfolio',
   remotes: ['resume-remote'],
-  // shared: (name, config) => {
-  //   if (['@angular/core', '@angular/router'].includes(name)) {
-  //     console.log(name, config)
-  //     return config
-  //   }
-  //   return false
-  // },
+  shared: (name, config) => {
+    if (name === 'smoothscroll-polyfill') {
+      return { ...config, eager: true }
+    }
+    return config
+  },
 }
